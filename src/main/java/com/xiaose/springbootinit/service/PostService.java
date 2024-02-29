8a6abdd6b6ed76756e9afd5fbbd4e99f1eff78ad
xiaose <imxiaose@qq.com>
2024-02-29 21:34:25 +0800

    简易版springboot模板,删除了wx与es相关数据

--- src/main/java/com/xiaose/springbootinit/service/PostService.java
+++ src/main/java/com/xiaose/springbootinit/service/PostService.java
@@ -1,12 +1,9 @@
 package com.xiaose.springbootinit.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.xiaose.springbootinit.model.dto.post.PostQueryRequest;
 import com.xiaose.springbootinit.model.entity.Post;
-import com.xiaose.springbootinit.model.vo.PostVO;
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * 帖子服务
@@ -31,29 +28,5 @@ public interface PostService extends IService<Post> {
      */
     QueryWrapper<Post> getQueryWrapper(PostQueryRequest postQueryRequest);
 
-    /**
-     * 从 ES 查询
-     *
-     * @param postQueryRequest
-     * @return
-     */
-    Page<Post> searchFromEs(PostQueryRequest postQueryRequest);
 
-    /**
-     * 获取帖子封装
-     *
-     * @param post
-     * @param request
-     * @return
-     */
-    PostVO getPostVO(Post post, HttpServletRequest request);
-
-    /**
-     * 分页获取帖子封装
-     *
-     * @param postPage
-     * @param request
-     * @return
-     */
-    Page<PostVO> getPostVOPage(Page<Post> postPage, HttpServletRequest request);
 }
